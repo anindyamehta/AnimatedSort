c536441dc41e1a3afed0f1c57442a0a298bf7c1a
anindyamehta <anindyamehta@gmail.com>
2023-07-14 22:33:16 -0400

    main update bubblesort, create quicksort

--- QuickSort.py
+++ QuickSort.py
@@ -1,16 +1,13 @@
 import pygame
 from sys import exit
 
-from Visualizer import Visualizer
-
 pygame.init()
 screen_width = 300
 screen_height = 215
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Bubble Sort")
+pygame.display.set_caption("Quick Sort")
 background_color = (0, 0, 0)  # black
 bar_color = (0, 255, 0)  # green
-
 
 def draw_bars(screen, background_color, bar_color, bar_width, bar_heights, screen_height):
     screen.fill(background_color)
@@ -18,37 +15,36 @@ def draw_bars(screen, background_color, bar_color, bar_width, bar_heights, scree
         pygame.draw.rect(screen, bar_color, (i * bar_width, screen_height - height, bar_width, height))
 
 
-def bubble_sort(arr):
+def quick_sort(arr):
     bar_width = screen_width / len(arr)  # less 10 for padding
     bar_heights = arr
+
     for i in range(0, len(arr)):
-        for j in range(i, len(arr)):
-            if arr[i] > arr[j]:
-                swap(arr, i, j)
-                draw_bars(screen, background_color, bar_color, bar_width, bar_heights, screen_height)
-                pygame.display.update()
-                pygame.time.delay(1000)
-
-
-def visualization(arr):
-    bar_width = screen_width / len(arr)  # less 10 for padding
-    bar_heights = arr
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit()
-
-        bubble_sort(arr)
-        draw_bars(screen, background_color, bar_color, bar_width, bar_heights, screen_height)
-        pygame.display.update()
-
+        smallest = arr[i]
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if (arr[j] < smallest):
+                smallest = arr[j]
+                min_idx = j
+        if (min_idx != i):
+            swap(arr, i, min_idx)
+            draw_bars(screen, background_color, bar_color, bar_width, bar_heights, screen_height)
+            pygame.display.update()
+            pygame.time.delay(100)
 
 def swap(arr, idx1, idx2):
     temp = arr[idx1]
     arr[idx1] = arr[idx2]
     arr[idx2] = temp
 
+def visualization(arr):
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+        quick_sort(arr)
 
 if __name__ == '__main__':
     visualization([13, 120, 83, 33, 74, 60, 50, 40, 30, 20, 10, 150, 200, 211, 188, 122, 41, 123, 55, 22])
